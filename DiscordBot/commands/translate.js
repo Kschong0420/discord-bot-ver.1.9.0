53f3dec634c1c2d075873331006e1899aee7c64c
Kschong0420 <79582179+Kschong0420@users.noreply.github.com>
2021-03-04 02:59:30 +0800

    Update translate.js

--- DiscordBot/commands/translate.js
+++ DiscordBot/commands/translate.js
@@ -7,13 +7,18 @@ module.exports = {
     async execute(client, message, args) {
         let language = args[0];
         let text = args.slice(1).join(" ");
+        let alias2 = args[2];
+        let alias5 = args[3]
 
         if (!language)
             return message.reply("What language am I suppose to translate to?");
-        if (language.length !== 2)
-            return message.reply(
-                "Language must be 2 letter alias. E.g `English > en`"
-            );
+        if (!language.length == 2)
+            if (!language.length == 5)
+                return message.reply(
+                    "All Language must be 2 letter alias. E.g `English > en`, except Simplified Chinese is `zh-cn` and Traditional Chinese is `zh-tw`."
+                );
+
+
         if (!text) return message.reply("What am I suppose to translate?");
 
         const result = await translate(text, { to: language });
@@ -27,4 +32,4 @@ module.exports = {
 
         message.channel.send(embed)
     }
-};
+};  
